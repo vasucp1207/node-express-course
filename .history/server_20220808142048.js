868d164fde97b4b55ed6092d3fc4e817549f2e17
Vasu <vasucp1207@gmail.com>
2022-08-08 14:36:22 +0530

    using postman for the endpoints

---
+++ .history/server_20220808142048.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'successfully got user.',
+        users: mockUserData
+    })
+})
+
+app.get('/users/:id', function(req, res){
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
+})
+
+app.post('/login', function(req, res){
+    const userName = 
+})
+
+app.listen(8000, () => {
+    console.log("server is running");
+})
---
+++ .history/server_20220808142247.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'successfully got user.',
+        users: mockUserData
+    })
+})
+
+app.get('/users/:id', function(req, res){
+    console.log(req.params.id);
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
+})
+
+app.post('/login', function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billy";
+    const mockPassword = "qwerty";
+
+    if(username == mockUsername && password == mockPassword){
+        res.json({
+            success: true,
+      	 	message: 'password and username match!',
+      	 	token: 'encrypted token goes here'
+        })
+    }
+    else{
+        res.json()
+    }
+})
+
+app.listen(8000, () => {
+    console.log("server is running");
+})
